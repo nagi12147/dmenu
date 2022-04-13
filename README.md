151b6a262ae6a1292c4af0ade166755ff17dc4c4
nagi12147 <nagi12147@tutanota.com>
2022-04-13 16:37:27 +0700

    new 2022/04/13

---
+++ README.md
@@ -0,0 +1,8 @@
+# dmenu
+dmenu is a dynamic menu for X, originally designed for dwm. It manages large numbers of user-defined menu items efficiently.
+
+## Latest commit
+2022-03-26 16:58    *fix UB with the function iscntrl()*
+
+## Credits
+[Original dmenu](https://tools.suckless.org/dmenu/) (suckless tools)
--- config.h
+++ config.h
@@ -3,15 +3,12 @@
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
-static const char *fonts[] = {
-	"monospace:style=bold:pixelsize=15",
-    "Noto Color Emoji:pixelsize=12"
-};
+static const char *fonts[] = { "monospace:style=bold:pixelsize=18", "M PLUS 1 Code:style=bold:size=10", "Noto Color Emoji:size=10" };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#a89984", "#1d2021" },
-	[SchemeSel]  = { "#ebdbb2", "#3c3836" },
+	[SchemeNorm] = { "#bbbbbb", "#222222" },
+	[SchemeSel] = { "#eeeeee", "#005577" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
