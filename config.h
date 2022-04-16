a54df8daaf3f6e18014331b9515e5d30401018d3
nagi12147 <nagi12147@tutanota.com>
2022-04-16 13:47:35 +0700

    change color to gruvbox

--- config.h
+++ config.h
@@ -7,8 +7,10 @@ static const char *fonts[] = { "monospace:style=bold:pixelsize=18", "M PLUS 1 Co
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
+	[SchemeNorm] = { "#a89984", "#282828" },
+	[SchemeSel] = { "#ebdbb2", "#282828" },
+//	[SchemeNorm] = { "#bbbbbb", "#222222" },
+//	[SchemeSel] = { "#eeeeee", "#005577" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
